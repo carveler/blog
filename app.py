ae2b750b3021b4c595f8f9574052f0a2c9d8e958
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-10 20:22:08 +0200

    feat: add css file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    with open("posts.json", "r") as posts_obj:
+        blog_posts = json.load(posts_obj)    
+    return render_template("index.html", posts=blog_posts)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
