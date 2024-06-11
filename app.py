8a814bc79bed937c905d585670419d30d4ddd52d
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-11 08:50:04 +0200

    feat: update blog post

--- app.py
+++ app.py
@@ -40,5 +40,27 @@ def delete(post_id):
     return "Post not found"
 
 
+def fetch_post_by_id(post_id):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    return None
+
+
+@app.route("/update/<post_id>", methods=["GET", "POST"])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == "POST":
+        post["title"] = request.form["title"]
+        post["author"] = request.form["author"]
+        post['content'] = request.form['content']
+        files.save_posts(posts)
+        return redirect(url_for("index"))
+    return render_template("update.html", post=post)
+
+
 if __name__ == "__main__":
     app.run()
